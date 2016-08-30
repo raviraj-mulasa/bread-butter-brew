ff485fd771f0550214c250af95cca339ce7230fd
RaviRaj Mulasa <rmulasa@apple.com>
2016-08-30 12:13:42 -0700

    IBTree added

--- src/main/java/edu/learn/me/tree/binary/IBtree.java
+++ src/main/java/edu/learn/me/tree/binary/IBtree.java
@@ -1,7 +1,18 @@
 package edu.learn.me.tree.binary;
 
 /**
- * Created by ravirajmulasa on 8/28/16.
+ * Created by ravirajmulasa on 8/30/16.
  */
-public class IBtree {
+public interface IBTree<T> {
+    void insert(T item);
+    BTreeNode<T> delete(T item);
+    BTreeNode<T> find(T item);
+
+
+    String preOrder(BTreeNode<T> node);
+    String postOrder(BTreeNode<T> node);
+    String inOrder(BTreeNode<T> node);
+
+    int height(BTreeNode<T> node);
+
 }
