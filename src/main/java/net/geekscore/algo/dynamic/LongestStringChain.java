943c9c8ecfbbf5f2a8217506eba0b92a53b87e86
RaviRaj <raviraj.mulasa@gmail.com>
2017-08-15 15:59:41 -0700

    Two Sigma.

---
+++ src/main/java/net/geekscore/algo/dynamic/LongestStringChain.java
@@ -0,0 +1,118 @@
+package net.geekscore.algo.dynamic;
+
+import java.util.*;
+
+/**
+ * Created by ravirajmulasa on 8/13/17.
+ *
+ * Given an array, words, of n word strings (words[0], words[1],..., words[n-1]),choose a word from it and, in each step,
+ * remove a single letter from the chosen word if and only if doing so yields another word that is already in the library.
+ * Each successive character removal should be performed on the result of the previous removal,
+ * and you cannot remove a character if the resulting string is not an element in words(see Explanation below for detail).
+ *
+ * The length of a string chain is the maximum number of strings in a chain of successive character removals.
+ *
+ * Complete the longestChainLength function in your editor.
+ * It has 1 parameter: an array of n strings, words, where the value of each element words;
+ * (where 0 <= i < n) is a word.
+ * It must return single integer denoting the length of the longest possible string chain in words.
+ *
+ * Sample Case 1: words = {"a", "b", "ba", "bca", "bda", "bdca"}
+ * Because "a" and "b" are single-character words,
+ * we cannot remove any characters from them as that would result in the empty string (which is not an element in words), so the length for both of these string chains is 1.
+ *
+ * The word "ba" can create two different string chains of length 2 ("ba" -> "a" and "ba" -> "b").
+ * This means our current longest string chain is 2.
+ *
+ * The word "bca" can create two different string chains of length 3 ("bca" -> "ba" -> "a" and "bca" -> "ba" -> "b").
+ * This means our current longest string chain is now 3.
+ *
+ * The word "bda" can create two different string chains of length 3 ("bda" -> "ba" -> "a" and "bda" -> "ba" -> "b").
+ * This means our current longest string chain is now 3.
+ *
+ * The word "bdca" can create four different string chains of length 4
+ * ("bdca" -> "bda" -> "ba" -> "a"
+ * , "bdca" -> "bda" -> "ba" -> "b"
+ * , "bdca" -> "bca" -> "ba" -> "a"
+ * , "bdca" -> "bca" -> "ba" -> "b").
+ * This means our current longest string chain is now 4.
+ *
+ */
+public class LongestStringChain {
+
+
+    public static void main(String[] args) {
+        final String[] words3 = {""}; // 0
+        final String[] words4 = {}; // 0
+        final String[] words = {"a", "b", "ba", "bca", "bda", "bdca"}; // 4
+        final String[] words1 = {"a", "ee"}; // 1
+        final String[] words2 = {"\b", "\b\b\b", "\b\b", "\b\b\b\b", "\b\b\b\b\b"};  // 5
+        final String[] words5 = {"a", "b", "c", "d", "bd", "bddb"}; // 2
+        final String[] words6 = {"a", "b", "c", "d", "bd", "bddb", "ddb"}; // 2
+        final String[] words7 = {"a", "b", "c", "d", "db", "bddb", "ddb"}; // 4
+        final String[] words8 = {"ddb", "eee", "efd"}; // 1
+        final String[] words9 = {"ddb", "dd", "eee", "ee", "efd"}; // 2
+        final String[] words10 = {"a", "b", "c", "d", "db", "bdebb", "dd"}; // 2
+
+        System.out.println(longestChainLength(null));
+        System.out.println(longestChainLength(words3));
+        System.out.println(longestChainLength(words4));
+        System.out.println(longestChainLength(words));
+        System.out.println(longestChainLength(words1));
+        System.out.println(longestChainLength(words2));
+        System.out.println(longestChainLength(words5));
+        System.out.println(longestChainLength(words6));
+        System.out.println(longestChainLength(words7));
+        System.out.println(longestChainLength(words8));
+        System.out.println(longestChainLength(words9));
+        System.out.println(longestChainLength(words10));
+    }
+
+    private static final int longestChainLength(String words[]) {
+        if(null == words || words.length == 0){
+            return 0;
+        }
+        final Map<String, Integer> chainLengthMap = new HashMap<>();
+        int longestChainLength = 0;
+        Arrays.stream(words).forEach(word -> chainLengthMap.put(word, 1));
+        for (final String word: words) {
+            longestChainLength = Math.max(longestChainLengthAtWord(word, chainLengthMap), longestChainLength);
+        }
+        return longestChainLength;
+
+    }
+
+    private static final int longestChainLengthAtWord(String word, final Map<String, Integer> chainLengthMap) {
+        if(null == word || word.length() == 0){
+            return 0;
+        }
+        Integer longestChainLength = chainLengthMap.putIfAbsent(word, 1);
+        if(null != longestChainLength) {
+            final Set<String> wordsSet  = chainLengthMap.keySet();
+//            Find longest chain length among  sub words
+            for (int i = 0; i < word.length(); i++) {
+                final String subWord = removeSingleLetter(i, word);
+                if (wordsSet.contains(subWord)) {
+                    Integer longestChainLengthSubWord = longestChainLengthAtWord(subWord, chainLengthMap);
+                    chainLengthMap.put(subWord, Math.min(longestChainLengthSubWord, subWord.length()));
+                    longestChainLength = Math.max(chainLengthMap.get(subWord) + 1, longestChainLength);
+                    chainLengthMap.put(word, longestChainLength);
+                }
+            }
+        }
+        return chainLengthMap.get(word);
+    }
+
+    private static final String removeSingleLetter(int index, String word) throws StringIndexOutOfBoundsException{
+        if(null == word || word.length() == 0){
+            return "";
+        }
+        if(index < 0 || index > word.length() - 1) {
+            throw new StringIndexOutOfBoundsException("Invalid Index");
+        }
+        return word.substring(0, index) + word.substring(index+1);
+    }
+
+
+
+}
---
+++ src/main/java/net/geekscore/disjointset/FriendsCircle.java
@@ -0,0 +1,70 @@
+package net.geekscore.disjointset;
+
+import java.util.*;
+
+/**
+ * Created by ravirajmulasa on 8/14/17.
+ */
+public class FriendsCircle {
+
+    public static void main(String[] args) {
+
+        final char[][] friends = {
+                {'Y','Y','N'}
+                , {'Y','Y','N'}
+                ,{'N','N','Y'}
+        };
+        final char[][] friends1 = {
+                {'Y','Y','N'}
+                , {'Y','Y','Y'}
+                ,{'N','Y','Y'}
+        };
+
+        final String[] friends2 = {"YYN", "YYN", "NNY"};
+        final String[] friends3 = {"YYN", "YYY", "NYY"};
+
+        System.out.println(friendCirclesCount(friends));
+        System.out.println(friendCirclesCount(friends1));
+        System.out.println(friendCircles(friends2));
+        System.out.println(friendCircles(friends3));
+    }
+
+    private static final int friendCircles(String[] friends) {
+        if(null == friends || friends.length == 0){
+            return 0;
+        }
+        final char[][] friends2DCharArray = new char[friends.length][];
+        for (int i = 0; i < friends.length; i++) {
+            friends2DCharArray[i] = friends[i].toCharArray();
+        }
+        return friendCirclesCount(friends2DCharArray);
+    }
+
+    private static final int friendCirclesCount(final char[][] friends) {
+        if (friends == null || friends.length < 1) {
+            return 0;
+        }
+        final boolean[] visited = new boolean[friends.length];
+        Arrays.fill(visited, Boolean.FALSE); // No node was visited yet.
+
+        int friendCircles = 0;
+
+        for (int i = 0; i < friends.length; i++) {
+            if (!visited[i]) {
+                friendCircles += 1;
+                visited[i]  = Boolean.TRUE;
+                friendCircle(friends, i, visited);
+            }
+        }
+        return friendCircles;
+    }
+
+    private static final void friendCircle(final char[][] friends, final Integer myId, final boolean[] visited) {
+        for (int i = 0; i < friends.length; i++) {
+            if (!visited[i] && myId != i && 'Y' == friends[myId][i]) {
+                visited[i] = Boolean.TRUE;
+                friendCircle(friends, i, visited);
+            }
+        }
+    }
+}
